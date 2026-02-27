039681b7d471cbfd2a101bf1cb12bec23f2f18a8
何璇 <15536672+Lfeng-hx@user.noreply.gitee.com>
2026-02-27 19:50:49 +0800

    白名单扩增

--- utils/skill_agent_constants.py
+++ utils/skill_agent_constants.py
@@ -4,5 +4,5 @@ SESSION_DIR_KEY_PREFIX = "skill:session_dir:"
 
 HISTORY_TRANSCRIPT_MAX_CHARS = 6000
 
-ALLOWED_COMMANDS = {"python", "pip", "node", "pandoc", "soffice", "pdftoppm", "npm", "npx", "bun"}
+ALLOWED_COMMANDS = {"python", "pip", "node", "pandoc", "soffice", "pdftoppm", "npm", "npx", "bun", "curl", "uvx", "wget", "git"}
 TEMP_SESSION_PREFIX = "dify-skill-"
